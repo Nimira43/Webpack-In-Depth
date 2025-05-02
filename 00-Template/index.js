a40becb2b36f3b57470c493344003d18b62119f1
Nimira43 <n.rabone72@aol.com>
2025-05-02 08:36:08 +0100

    Created listItems array.

--- 00-Template/index.js
+++ 00-Template/index.js
@@ -2,5 +2,6 @@ function buttonClicked() {
   const el = document.getElementById('header')
   el.innerHTML = 'Code has been updated'
 
+  const listItems = ['apples', 'bread', 'coffee']
   const ul = document.getElementById('shoppingList')
 }
