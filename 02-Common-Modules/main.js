aef5beb524c63c4d27a9050c7b36136c47ac9c1e
Nimira43 <n.rabone72@aol.com>
2025-05-02 10:12:35 +0100

    Importing add and addAndMultiple

--- 02-Common-Modules/main.js
+++ 02-Common-Modules/main.js
@@ -1,5 +1,6 @@
-const add = require('./functions/add')
+const { add, addAndMultiply} = require('./functions/add')
 const subtract = require('./functions/subtract')
 
 console.log(add(5, 7))
 console.log(subtract(5, 7))
+console.log(addAndMultiply(4, 4, 4))
