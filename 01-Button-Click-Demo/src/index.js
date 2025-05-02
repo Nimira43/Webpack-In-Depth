2c2132499ef4bd56b341cf1ff61a7ce11afe2d65
Nimira43 <n.rabone72@aol.com>
2025-05-02 09:52:15 +0100

    Project clean up. Getting ready for next mini project.

--- 01-Button-Click-Demo/src/index.js
+++ 01-Button-Click-Demo/src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 
 document.getElementById('button1').addEventListener('click', function () {
   const el = document.getElementById('header')
-  el.innerHTML = 'Code has been updated'
+  el.innerHTML = 'Button Clicked!!! Code has been updated'
   const listItems = ['apples', 'bread', 'coffee']
   const ul = document.getElementById('shoppingList')
 
